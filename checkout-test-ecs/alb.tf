2d424fb38d0ab5541e182618a6dd6d49cbdfed6e
Ashutosh Rathore <ashutoshrathore@Ashutoshs-MacBook-Air.local>
2022-06-04 21:19:38 +0530

    fixing error

--- checkout-test-ecs/alb.tf
+++ checkout-test-ecs/alb.tf
@@ -55,12 +55,12 @@ resource "aws_lb_target_group" "alb_target_group" {
 }
 
 resource "aws_lb_listener" "web-listener" {
-  load_balancer_arn = aws_lb.test-lb.arn
+  load_balancer_arn = aws_lb.alb-ecs.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = aws_alb_target_group.alb_target_group.arn
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
 }
 
--- checkout-test-ecs/vpc.tf
+++ checkout-test-ecs/vpc.tf
@@ -1,6 +1,5 @@
 resource "aws_vpc" "vpc_checkout_ecs" {
   cidr_block       = "10.0.0.0/16"
-  instance_tenancy = default
   enable_dns_hostnames = true
   enable_dns_support = true
 
