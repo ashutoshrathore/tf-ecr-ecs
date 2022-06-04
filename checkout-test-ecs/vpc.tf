7070b6728802af401d82189f3b63592cafa6305c
Ashutosh Rathore <ashutoshrathore@Ashutoshs-MacBook-Air.local>
2022-06-04 23:24:29 +0530

    modifying network

--- checkout-test-ecs/vpc.tf
+++ checkout-test-ecs/vpc.tf
@@ -75,11 +75,11 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private_a" {
-  subnet_id      = [aws_subnet.private_subnet_a.id]
+  subnet_id      = aws_subnet.private_subnet_a.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private_b" {
-  subnet_id      = [aws_subnet.private_subnet_b.id]
+  subnet_id      = aws_subnet.private_subnet_b.id
   route_table_id = aws_route_table.private.id
 }
