c1d132839474ae9bcb4f405f0a9a266ebcbd2462
Ashutosh Rathore <ashutoshrathore@Ashutoshs-MacBook-Air.local>
2022-06-04 19:41:41 +0530

    adding tags

--- vpc-ecs/versions.tf
+++ vpc-ecs/versions.tf
@@ -9,11 +9,5 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   access_key=var.AWS_ACCESS_KEY
-  secret_key=var.AWS_SECRET_KEY
-  default_tags {
-    tags = {
-     Environment = "Test"
-     Owner       = "DevOps - checkout "
-    }
-  } 
+  secret_key=var.AWS_SECRET_KEY 
 }
