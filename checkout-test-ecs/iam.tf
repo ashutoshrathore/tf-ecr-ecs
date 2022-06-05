0118c76185b0982a6393c459e444a80ca35af7ca
Ashutosh Rathore <ashutoshrathore@Ashutoshs-MacBook-Air.local>
2022-06-05 13:32:39 +0530

    with fargate

--- checkout-test-ecs/iam.tf
+++ checkout-test-ecs/iam.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 
 # ECS task execution role
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = var.ecs_task_execution_role_name
+  name               = "ecsTaskExecution"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 }
 
